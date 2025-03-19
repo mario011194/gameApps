76900c80eb7be202855613214a87dad358cf5950
mario alexander <mario.alexander@danamon.co.id>
2025-03-19 11:26:48 +0700

    Initial Commit

---
+++ ProjectDicoding3/ProjectDicoding3App.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectDicoding3App.swift
+//  ProjectDicoding3
+//
+//  Created by Mario Alexander on 3/19/25.
+//
+
+import SwiftUI
+
+@main
+struct ProjectDicoding3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ProjectDicoding3Tests/ProjectDicoding3Tests.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectDicoding3Tests.swift
+//  ProjectDicoding3Tests
+//
+//  Created by Mario Alexander on 3/19/25.
+//
+
+import Testing
+@testable import ProjectDicoding3
+
+struct ProjectDicoding3Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
